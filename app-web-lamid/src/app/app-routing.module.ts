b7b0b798928c78c1d411fb6befe9c449400964b1
ivamgf <programador.ivam@gmail.com>
2022-12-20 19:46:57 -0300

    first commit

---
+++ app-web-lamid/src/app/app-routing.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {ContactsComponent} from "./pages/contacts/contacts.component";
+import {GalleryComponent} from "./pages/gallery/gallery.component";
+import {IdentityComponent} from "./pages/identity/identity.component";
+import {InfrastructureComponent} from "./pages/infrastructure/infrastructure.component";
+import {ProjectsComponent} from "./pages/projects/projects.component";
+import {TeamComponent} from "./pages/team/team.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pages/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pages/home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent
+  },
+  {
+    path: 'identity',
+    component: IdentityComponent
+  },
+  {
+    path: 'infrastructure',
+    component: InfrastructureComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
