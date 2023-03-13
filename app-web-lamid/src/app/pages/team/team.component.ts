7913a3615378ecaedd522a48f41e71d91a35fe07
Ivam <ivam.developer@gmail.com>
2023-03-13 13:16:56 -0300

    add team page

--- app-web-lamid/src/app/pages/team/team.component.ts
+++ app-web-lamid/src/app/pages/team/team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-team',
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
 
+  }
+
+  breadcrumbHome() {
+    this.router.navigate(['/pages/home']);
   }
 
 }
